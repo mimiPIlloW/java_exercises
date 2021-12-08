675505e95e217a887f39b95ff210173abcf8f41f
mimiPillow <erenstonxemin@gmail.com>
2021-12-08 13:16:46 +0200

    basic_part1 - ex1

---
+++ src/com/java/exercises/practice/solution/basic_part1/ex1/Hello.java
@@ -0,0 +1,7 @@
+package com.java.exercises.practice.solution.basic_part1.ex1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello\nAlexandra Abramov");
+    }
+}
