41f8feea5a8b92b7f2c48532035f37ca3c0f7784
mimiPIllow <erenstonxemin@gmail.com>
2022-01-31 12:08:25 +0200

    chapter2_testautomationu

---
+++ src/com/java/testautomationu/chapter2/GrossPayCalculator.java
@@ -0,0 +1,20 @@
+package com.java.testautomationu.chapter2;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number of hours the employee worked.");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+        System.out.println("Enter the employee's pay rate.");
+        double rate = scanner.nextDouble();
+        scanner.close();
+
+        double grossPay = hours * rate;
+
+        System.out.println("$" + grossPay);
+    }
+}
