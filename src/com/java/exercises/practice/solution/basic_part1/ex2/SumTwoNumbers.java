48a123e50f757c8f97e22bdf40e78cf89bc16c15
mimiPIllow <erenstonxemin@gmail.com>
2022-01-31 12:07:49 +0200

    basic_part1_ex2

---
+++ src/com/java/exercises/practice/solution/basic_part1/ex2/SumTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.java.exercises.practice.solution.basic_part1.ex2;
+
+public class SumTwoNumbers {
+    private static int sumOne = 74;
+    private static int sumTwo = 36;
+
+    public static void main(String[] args) {
+        int result = sumOne + sumTwo;
+        System.out.println(result);
+    }
+}
