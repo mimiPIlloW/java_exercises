aae60cd341c1dd06dec14a89123edee7becb9762
mimiPIllow <erenstonxemin@gmail.com>
2022-01-31 12:54:28 +0200

    w3resource_string_ex1

---
+++ src/com/java/exercises/practice/solution/string/ex_1/FoundChar.java
@@ -0,0 +1,26 @@
+package com.java.exercises.practice.solution.string.ex_1;
+
+import java.util.Scanner;
+
+public class FoundChar {
+    public static void main(String[] args) {
+
+        System.out.println("Given index within the String");
+        System.out.println("Write the text");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+
+        System.out.println("Напишите индекс1");
+        int ind1 = scanner.nextInt();
+
+        System.out.println("Напишите индекс2");
+        int ind2 = scanner.nextInt();
+
+        int resultInd1 = str.charAt(ind1);
+        int resultInd2 = str.charAt(ind2);
+
+        System.out.println("The character at position " + ind1 + " is " + (char) resultInd1);
+        System.out.println("The character at position " + ind2 + " is " + (char) resultInd2);
+    }
+}
+
