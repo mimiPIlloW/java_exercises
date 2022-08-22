aedf1b2cb327f841e70e2803bb8819ec1446d5ec
mimiPillow <erenstonxemin@gmail.com>
2022-08-22 12:21:17 +0300

    w3resource collection exercise 4

---
+++ src/com/java/exercises/practice/solution/collection/arrayList/ArrayList4.java
@@ -0,0 +1,17 @@
+package com.java.exercises.practice.solution.collection.arrayList;
+
+import java.util.ArrayList;
+
+public class ArrayList4 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList= new ArrayList<>();
+
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
+
+        System.out.println(arrayList.get(2));
+    }
+}
